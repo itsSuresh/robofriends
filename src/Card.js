24a987c11c22666cea3c2f5cd15a6c8cc8a7bb6d
sureshpc <itssuresh.m@gmail.com>
2019-05-07 07:03:45 +0530

    robofriends update till APP.js

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Card = ({name,email,id})=>{
+      return(
+        <div className="bg-light-green dib ma3 pa3 bw2 shadow-5 grow tc">
+            <img src={`https://robohash.org/${id}?200x200`} alt="robo"></img>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+      
+    );
+}
+
+export default Card;
